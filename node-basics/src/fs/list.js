a7663de51b6bcbd1860a7ca33b6099ed5ef5fce8
polinsky96 <janpolin96@gmail.com>
2022-12-03 22:57:56 +0300

    done list.js

--- node-basics/src/fs/list.js
+++ node-basics/src/fs/list.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readdir, access } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = path.join(__dirname, './files');
+    const myError = 'FS operation failed';
+
+    try {
+        access(dirPath, (err) => {
+            if (err) {
+                throw Error(myError)
+            }
+        });
+
+        readdir(dirPath, (err, files) => {
+            if (err) throw Error(myError);
+
+            console.log(files);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await list();
