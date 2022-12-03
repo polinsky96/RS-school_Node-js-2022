92ab1919d8f1d5827687fb345f48b4db89cff065
polinsky96 <janpolin96@gmail.com>
2022-12-04 00:22:16 +0300

    done env.js

--- node-basics/src/cli/env.js
+++ node-basics/src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    console.log(
+        Object.entries(process.env)
+            .filter(variable => variable[0].includes('RSS_'))
+            .map(variable => `${variable[0]}=${variable[1]}`)
+            .join('; ')
+    );
 };
 
 parseEnv();
