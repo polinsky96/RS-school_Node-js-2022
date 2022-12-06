57108dfb5db427eee2200a06eab08fff2c752894
polinsky96 <janpolin96@gmail.com>
2022-12-06 14:35:45 +0300

    done read.js

--- node-basics/src/streams/read.js
+++ node-basics/src/streams/read.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+import { createReadStream } from 'node:fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(path.join(__dirname, './files/fileToRead.txt'));
+
+    readable.on('data', (chunc) => {
+        stdout.write(chunc)
+    })
 };
 
 await read();
