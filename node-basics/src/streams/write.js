22071df9fd26a949596b34b33dfd157491af8028
polinsky96 <janpolin96@gmail.com>
2022-12-06 14:58:09 +0300

    done write.js

--- node-basics/src/streams/write.js
+++ node-basics/src/streams/write.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeable = createWriteStream(path.join(__dirname, './files/fileToWrite.txt'));
+
+    stdin.on('data', (data) => {
+        writeable.write(data);
+    })
 };
 
 await write();
